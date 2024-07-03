120c6068517c220609c4140b8dbca13665aa3045
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-07-03 15:56:34 +0200

    FakerSeeder funzionante

---
+++ database/seeders/FakerSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class FakerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $newTrain = new Train();
+
+        $newTrain->company = $faker->word();
+        $newTrain->departure_station = $faker->word();
+        $newTrain->arrival_station = $faker->word();
+        $newTrain->departure_time = $faker->dateTime();
+        $newTrain->arrival_time = $faker->dateTime();
+        $newTrain->train_code = $faker->randomNumber(9, true);
+        $newTrain->number_of_carriages = $faker->randomNumber(9, false);
+        $newTrain->on_time = $faker->boolean();
+        $newTrain->cancelled = $faker->boolean();
+
+        $newTrain->save();
+    }
+}
