a778f2c4480fd90128ad3aa93600555e8265c4b1
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-07-03 15:10:38 +0200

    creazione del model train e del controller

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $trains = Train::all();
+        $data = ['trains' => $trains];
+        return view('home', $data);
+    }
+}
