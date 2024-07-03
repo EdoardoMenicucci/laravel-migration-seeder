b05350f4bec52d1b3204388c2d025497403b4eda
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-07-03 15:41:04 +0200

    foreach con if per stampare l'unico treno del database in partenza oggi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+	@foreach ($trains as $train)
+		@if (str_contains($train->arrival_time, '2024-07-03'))
+			<div>
+				<h1>Il Treno {{ $train->train_code }} sta per lasciare {{ $train->departure_station }} alle
+					{{ $train->departure_time }}.
+			</div>
+		@endif
+	@endforeach
 @endsection
